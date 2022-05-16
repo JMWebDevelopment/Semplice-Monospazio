8350a0c9abdfaf481caa36518230e94f64f6f183
Jacob Martella <me@jacobmartella.com>
2022-05-16 17:50:41 -0500

    Started the process of rebuilding the theme for WP 6.0.

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * This file adds functions to the Semplice Monospazio Theme for WordPress.
+ * This file adds functions to the JM Web Development FSE Starter Theme for WordPress.
  *
- * @package Semplice Monospazio
+ * @package JM Web Development FSE Starter Theme
  * @author  Jacob Martella Web Development
  * @license GNU General Public License v2 or later
  * @link    https://jacobmartella.com/
  */
 
-if ( ! function_exists( 'semplice_monospazio_setup' ) ) {
+if ( ! function_exists( 'jm_web_dev_fse_starter_theme_setup' ) ) {
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -20,9 +20,9 @@ if ( ! function_exists( 'semplice_monospazio_setup' ) ) {
 	 *
 	 * @return void
 	 */
-	function semplice_monospazio_setup() {
+	function jm_web_dev_fse_starter_theme_setup() {
 		// Make theme available for translation.
-		load_theme_textdomain( 'semplice-monospazio', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'jm-web-dev-fse-starter-theme', get_template_directory() . '/languages' );
 
 		// Add support for Block Styles.
 		add_theme_support( 'wp-block-styles' );
@@ -34,7 +34,7 @@ if ( ! function_exists( 'semplice_monospazio_setup' ) ) {
 		add_editor_style(
 			array(
 				'./assets/css/editor-styles.min.css',
-				semplice_monospazio_fonts_url(),
+				jm_web_dev_fse_starter_theme_fonts_url(),
 			)
 		);
 
@@ -45,52 +45,113 @@ if ( ! function_exists( 'semplice_monospazio_setup' ) ) {
 		remove_theme_support( 'core-block-patterns' );
 	}
 }
-add_action( 'after_setup_theme', 'semplice_monospazio_setup' );
+add_action( 'after_setup_theme', 'jm_web_dev_fse_starter_theme_setup' );
 
 // Enqueue style sheet.
-add_action( 'wp_enqueue_scripts', 'semplice_monospazio_enqueue_style_sheet' );
-function semplice_monospazio_enqueue_style_sheet() {
+add_action( 'wp_enqueue_scripts', 'jm_web_dev_fse_starter_theme_enqueue_style_sheet' );
+function jm_web_dev_fse_starter_theme_enqueue_style_sheet() {
 
-	wp_enqueue_style( 'semplice-monospazio', get_template_directory_uri() . '/assets/css/global.min.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'jm-web-dev-fse-starter-theme', get_template_directory_uri() . '/assets/css/styles.min.css', array(), wp_get_theme()->get( 'Version' ) );
 
 }
 
 // Enqueue fonts.
-add_action( 'wp_enqueue_scripts', 'semplice_monospazio_enqueue_fonts' );
-function semplice_monospazio_enqueue_fonts() {
+add_action( 'wp_enqueue_scripts', 'jm_web_dev_fse_starter_theme_enqueue_fonts' );
+function jm_web_dev_fse_starter_theme_enqueue_fonts() {
 
-	wp_enqueue_style( 'semplice-monospazio-theme-fonts', semplice_monospazio_fonts_url(), array(), null );
+	wp_enqueue_style( 'jm-web-dev-fse-starter-theme-fonts', jm_web_dev_fse_starter_theme_fonts_url(), array(), null );
 
 }
 
 // Define fonts.
-function semplice_monospazio_fonts_url() {
+function jm_web_dev_fse_starter_theme_fonts_url() {
 
-	// Allow child themes to disable to the default Semplice Monospazio fonts.
-	$dequeue_fonts = apply_filters( 'semplice_monospazio_dequeue_fonts', false );
+	// Allow child themes to disable to the default JM Web Development FSE Starter Theme fonts.
+	$dequeue_fonts = apply_filters( 'jm_web_dev_fse_starter_theme_dequeue_fonts', false );
 
 	if ( $dequeue_fonts ) {
 		return '';
 	}
 
-	$fonts = [
-		'family=Ubuntu+Mono:wght@400;700;',
-	];
+	$fonts = [];
 
 	// Make a single request for all Google Fonts.
-	return esc_url_raw( 'https://fonts.googleapis.com/css?' . implode( '&', array_unique( $fonts ) ) . '&display=swap' );
+	return esc_url_raw( 'https://fonts.googleapis.com/css2?' . implode( '&', array_unique( $fonts ) ) . '&display=swap' );
 }
 
-function semplice_monospazio_search_title() {
+function jm_web_dev_fse_starter_theme_search_title() {
 	if ( isset( $_GET['s'] ) ) {
 		$search_term = sanitize_text_field( wp_unslash( $_GET['s'] ) );
 		/* translators: %s: Search term. */
-		return isset( $search_term ) ? sprintf( esc_html__( 'Search results for "%s"', 'semplice-monospazio' ), esc_html( $search_term ) ) : __( 'Search results', 'semplice-monospazio' );
+		return isset( $search_term ) ? sprintf( esc_html__( 'Search results for "%s"', 'jm-web-dev-fse-starter-theme' ), esc_html( $search_term ) ) : __( 'Search results', 'jm-web-dev-fse-starter-theme' );
 	}
 }
 
 // Include block styles.
-require get_template_directory() . '/inc/block-styles.php';
+register_block_style(
+	'core/group',
+	array(
+		'name'  => 'full-height',
+		'label' => __( 'Full-height', 'jm-web-dev-fse-starter-theme' ),
+	)
+);
+
 
 // Include block patterns.
-require get_template_directory() . '/inc/block-patterns.php';
+/**
+ * Registers block patterns, categories, and type.
+ *
+ * @since 1.0.0
+ */
+function jm_web_dev_fse_starter_theme_register_block_patterns() {
+
+	if ( function_exists( 'register_block_pattern_category_type' ) ) {
+		register_block_pattern_category_type(
+			'jm-web-dev-fse-starter-theme',
+			array(
+				'label' => __( 'JM Web Dev FSE Starter Theme', 'jm-web-dev-fse-starter-theme' ),
+			)
+		);
+	}
+
+	$block_pattern_categories = array(
+		'jm-web-dev-fse-starter-theme-404'  => array(
+			'label'         => __( '404', 'jm-web-dev-fse-starter-theme' ),
+			'categoryTypes' => array( 'jm-web-dev-fse-starter-theme' ),
+		),
+		'jm-web-dev-fse-starter-theme-footer'  => array(
+			'label'         => __( 'Footer', 'jm-web-dev-fse-starter-theme' ),
+			'categoryTypes' => array( 'jm-web-dev-fse-starter-theme' ),
+		),
+		'jm-web-dev-fse-starter-theme-header'  => array(
+			'label'         => __( 'Header', 'jm-web-dev-fse-starter-theme' ),
+			'categoryTypes' => array( 'jm-web-dev-fse-starter-theme' ),
+		),
+		'jm-web-dev-fse-starter-theme-search'  => array(
+			'label'         => __( 'Search', 'jm-web-dev-fse-starter-theme' ),
+			'categoryTypes' => array( 'jm-web-dev-fse-starter-theme' ),
+		),
+	);
+
+	/**
+	 * Filters the theme block pattern categories.
+	 *
+	 * @since 0.8.0
+	 *
+	 * @param array[] $block_pattern_categories {
+	 *     An associative array of block pattern categories, keyed by category name.
+	 *
+	 *     @type array[] $properties {
+	 *         An array of block category properties.
+	 *
+	 *         @type string $label A human-readable label for the pattern category.
+	 *     }
+	 * }
+	 */
+	$block_pattern_categories = apply_filters( 'jm_web_dev_fse_starter_theme_block_pattern_categories', $block_pattern_categories );
+
+	foreach ( $block_pattern_categories as $name => $properties ) {
+		register_block_pattern_category( $name, $properties );
+	}
+}
+add_action( 'init', 'jm_web_dev_fse_starter_theme_register_block_patterns', 9 );
