ee2ce4e95a55215db24fc6eba874aa16b073ee96
Jacob Martella <me@jacobmartella.com>
2022-05-17 21:48:07 -0500

    Added the page template and started the sidebar template.
    Addresses #8 #9

---
+++ patterns/sidebar-widget.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: Sidebar Widget.
+ * Slug: jm-web-dev-fse-starter-theme/sidebar-widget
+ * Categories: jm-web-dev-fse-starter-theme-sidebar
+ * Viewport Width: 1280
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"className":"widget"} -->
+<div class="wp-block-group widget"><!-- wp:heading -->
+<h2><?php esc_html_e( 'Widget Title', 'jm-web-dev-fse-starter-theme' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:separator {"align":"center","className":"is-style-wide"} -->
+<hr class="wp-block-separator aligncenter has-alpha-channel-opacity is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
